ac9c51a21bca1734002611f9c36f23ccb7b7ea32
zahrayasmne123 <132264334+zahrayasmne123@users.noreply.github.com>
2024-10-21 13:51:10 +0100

    fix: update get device and place into a main function

--- scripts/2. Train/1. Train Global Model.py
+++ scripts/2. Train/1. Train Global Model.py
@@ -13,42 +13,64 @@ from imuposer.config import Config, amass_combos
 from imuposer.models.utils import get_model
 from imuposer.datasets.utils import get_datamodule
 from imuposer.utils import get_parser
+from imuposer.get_device import get_device
 
-# set the random seed
-seed_everything(42, workers=True)
 
-parser = get_parser()
-args = parser.parse_args()
-combo_id = args.combo_id
-fast_dev_run = args.fast_dev_run
-_experiment = args.experiment
+def main():
 
-# %%
-config = Config(experiment=f"{_experiment}_{combo_id}", model="GlobalModelIMUPoser",
-                project_root_dir="../../", joints_set=amass_combos[combo_id], normalize="no_translation",
-                r6d=True, loss_type="mse", use_joint_loss=True, device="0") 
+    # set the random seed
+    seed_everything(42, workers=True)
 
-# %%
-# instantiate model and data
-model = get_model(config)
-datamodule = get_datamodule(config)
-checkpoint_path = config.checkpoint_path 
+    parser = get_parser()
+    args = parser.parse_args()
+    combo_id = args.combo_id
+    fast_dev_run = args.fast_dev_run
+    _experiment = args.experiment
 
-# %%
-wandb_logger = WandbLogger(project=config.experiment, save_dir=checkpoint_path)
+    # %%
+    device = get_device()
+    print(f"Using device: {device}")
 
-early_stopping_callback = EarlyStopping(monitor="validation_step_loss", mode="min", verbose=False,
-                                        min_delta=0.00001, patience=5)
-checkpoint_callback = ModelCheckpoint(monitor="validation_step_loss", mode="min", verbose=False, 
-                                      save_top_k=5, dirpath=checkpoint_path, save_weights_only=True, 
-                                      filename='epoch={epoch}-val_loss={validation_step_loss:.5f}')
+    config = Config(experiment=f"{_experiment}_{combo_id}", model="GlobalModelIMUPoser",
+                    project_root_dir="../../", joints_set=amass_combos[combo_id], normalize="no_translation",
+                    r6d=True, loss_type="mse", use_joint_loss=True, device=device)
 
-trainer = pl.Trainer(fast_dev_run=fast_dev_run, logger=wandb_logger, max_epochs=1000, accelerator="gpu", devices=[0],
-                     callbacks=[early_stopping_callback, checkpoint_callback], deterministic=True)
+    # %%
+    # instantiate model and data
+    model = get_model(config)
+    datamodule = get_datamodule(config)
+    checkpoint_path = config.checkpoint_path 
 
-# %%
-trainer.fit(model, datamodule=datamodule)
+    # %%
+    wandb_logger = WandbLogger(project=config.experiment, save_dir=checkpoint_path)
 
-# %%
-with open(checkpoint_path / "best_model.txt", "w") as f:
-    f.write(f"{checkpoint_callback.best_model_path}\n\n{checkpoint_callback.best_k_models}")
+    early_stopping_callback = EarlyStopping(monitor="validation_step_loss", mode="min", verbose=False,
+                                            min_delta=0.00001, patience=5)
+    checkpoint_callback = ModelCheckpoint(monitor="validation_step_loss", mode="min", verbose=False, 
+                                        save_top_k=5, dirpath=checkpoint_path, save_weights_only=True, 
+                                        filename='epoch={epoch}-val_loss={validation_step_loss:.5f}')
+
+    # trainer = pl.Trainer(fast_dev_run=fast_dev_run, logger=wandb_logger, max_epochs=1000, accelerator="gpu", devices=[0],
+    #                      callbacks=[early_stopping_callback, checkpoint_callback], deterministic=True)
+
+    if device.type == 'mps':
+        trainer = pl.Trainer(fast_dev_run=fast_dev_run, logger=wandb_logger, max_epochs=1000, 
+                            accelerator="mps", devices=[0],
+                            callbacks=[early_stopping_callback, checkpoint_callback], deterministic=True)
+    elif device.type == 'cuda':
+        trainer = pl.Trainer(fast_dev_run=fast_dev_run, logger=wandb_logger, max_epochs=1000, 
+                            accelerator="gpu", devices=[0],
+                            callbacks=[early_stopping_callback, checkpoint_callback], deterministic=True)
+    else:
+        trainer = pl.Trainer(fast_dev_run=fast_dev_run, logger=wandb_logger, max_epochs=1000, 
+                            accelerator="cpu",
+                            callbacks=[early_stopping_callback, checkpoint_callback], deterministic=True)
+    # %%
+    trainer.fit(model, datamodule=datamodule)
+
+    # %%
+    with open(checkpoint_path / "best_model.txt", "w") as f:
+        f.write(f"{checkpoint_callback.best_model_path}\n\n{checkpoint_callback.best_k_models}")
+
+if __name__ == "__main__":
+    main()
